4a924f9fa4d6a275b6f6c202f44194fff33ca5f9
Felinto <emersong20.email@gmail.com>
2022-02-19 19:54:09 +0000

    🚧 add support to bull

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: 'redis',
+        port: 6379,
+      },
+    }),
+  ],
 })
 export class AppModule {}
