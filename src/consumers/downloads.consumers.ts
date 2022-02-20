0f9db6872ee34c1073096082f5bc9c4ccee3e107
Felinto <emersong20.email@gmail.com>
2022-02-20 22:22:26 +0000

    ♻️ use logger instead console.log

--- src/consumers/downloads.consumers.ts
+++ src/consumers/downloads.consumers.ts
@@ -1,11 +1,12 @@
 import { Job, Queue } from 'bull';
-import { Cron, CronExpression } from '@nestjs/schedule';
 import {
   InjectQueue,
   OnQueueCompleted,
   Process,
   Processor,
 } from '@nestjs/bull';
+import { Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 import { DOWNLOADS_QUEUE } from '@/consts/queues';
 import { DownloadsService } from '@/services/downloads.service';
@@ -13,6 +14,8 @@ import { GLOBAL_DOWNLOADS_CONCURRENCY } from '@/consts/app';
 
 @Processor(DOWNLOADS_QUEUE)
 export class DownloadsConsumer {
+  private readonly logger = new Logger(DownloadsConsumer.name);
+
   constructor(
     @InjectQueue(DOWNLOADS_QUEUE)
     private downloadsRequestQueue: Queue,
@@ -28,7 +31,7 @@ export class DownloadsConsumer {
 
   @Cron(CronExpression.EVERY_HOUR)
   async pullJobs() {
-    console.log('Pulling jobs to queue from Database...');
+    this.logger.verbose('Pulling jobs to queue from Database...');
     if ((await this.jobsActiveQuotaLeft()) < 1) {
       return;
     }
