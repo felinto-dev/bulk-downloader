1e67ab1a20d66036d26a59bb2337dcf955ede8e5
Felinto <emersong20.email@gmail.com>
2022-02-20 08:14:52 +0000

    ⚡️ improve build time

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import {
 import { AppModule } from './app.module';
 import { configureApp } from './configure';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
@@ -14,5 +16,10 @@ async function bootstrap() {
   );
   configureApp(app);
   await app.listen(3000, '0.0.0.0');
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
