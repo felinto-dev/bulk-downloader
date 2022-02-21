549d08c564d7adc3e840a4655a3f97f05bfb816e
Felinto <emersong20.email@gmail.com>
2022-02-21 05:11:35 +0000

    ⚰️ remove dead code

--- src/services/hosters.service.ts
+++ src/services/hosters.service.ts
@@ -1,14 +1,10 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
 import { HostersRepository } from '@/repositories/hosters.repository';
 
 @Injectable()
-export class HostersService implements OnModuleInit {
+export class HostersService {
   constructor(private readonly hostersRepository: HostersRepository) {}
-
-  async onModuleInit() {
-    await this.getInactiveHosters();
-  }
 
   async getInactiveHosters() {
     const hosters = await this.hostersRepository.getInactiveHosters();
