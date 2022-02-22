645b880efc6018300cb80287f52e754e5071d0f8
Felinto <emersong20.email@gmail.com>
2022-02-22 01:10:19 +0000

    🚚 rename utils

--- src/utils/objects.ts
+++ src/utils/objects.ts
@@ -1,6 +1,8 @@
-export const sanitizeObject = (obj: Record<string, unknown>) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  return Object.fromEntries(Object.entries(obj).filter(([_, v]) => v != null));
+export const removeNullObjectValues = (obj: Record<string, unknown>) => {
+  return Object.entries(obj).reduce(
+    (a, [k, v]) => (v === null ? a : ((a[k] = v), a)),
+    {},
+  );
 };
 
 export interface SimpleObject {
@@ -12,7 +14,7 @@ export const subtractObjects = (
   objA: SimpleObject,
   objB: SimpleObject,
 ): SimpleObject => {
-  return Object.keys(sanitizeObject(objA)).reduce((a, k) => {
+  return Object.keys(removeNullObjectValues(objA)).reduce((a, k) => {
     a[k] = objA[k] - objB[k];
     return a;
   }, {});
