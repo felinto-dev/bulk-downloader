82cd68649c2d1abbe1c967e10ab91736c49395cc
Felinto <emersong20.email@gmail.com>
2022-02-22 03:37:08 +0000

    🚧 add TODO

--- src/configs/config-module.config.ts
+++ src/configs/config-module.config.ts
@@ -2,6 +2,7 @@ import { ConfigModuleOptions } from '@nestjs/config';
 
 import queueConfig from './queue.config';
 
+// TODO: Use TypeScript type-checking for configService using InjectKey()
 export const configModuleConfig: ConfigModuleOptions = {
   ignoreEnvFile: true,
   cache: true,
