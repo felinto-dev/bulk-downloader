8815da4ab6b5522989a8da1dea82704405f15356
Felinto <emersong20.email@gmail.com>
2022-02-22 03:32:31 +0000

    ♻️ improve code legiblity

--- src/consumers/downloads.consumers.ts
+++ src/consumers/downloads.consumers.ts
@@ -1,10 +1,10 @@
-import { Job, Queue } from 'bull';
 import {
   InjectQueue,
   OnQueueCompleted,
   Process,
   Processor,
 } from '@nestjs/bull';
+import { Job, Queue } from 'bull';
 import { Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 
@@ -17,29 +17,23 @@ export class DownloadsConsumer {
   private readonly logger = new Logger(DownloadsConsumer.name);
 
   constructor(
-    @InjectQueue(DOWNLOADS_QUEUE)
-    private downloadsRequestQueue: Queue,
+    @InjectQueue(DOWNLOADS_QUEUE) private readonly queue: Queue,
     private readonly downloadsService: DownloadsService,
   ) {}
 
-  async jobsActiveQuotaLeft() {
-    return (
-      GLOBAL_DOWNLOADS_CONCURRENCY -
-      (await this.downloadsRequestQueue.getActiveCount())
-    );
+  private async jobsActiveQuotaLeft() {
+    return GLOBAL_DOWNLOADS_CONCURRENCY - (await this.queue.getActiveCount());
   }
 
   @Cron(CronExpression.EVERY_HOUR)
   async pullJobs() {
     this.logger.verbose('Pulling jobs to queue from Database...');
-    if ((await this.jobsActiveQuotaLeft()) < 1) {
+    if ((await this.jobsActiveQuotaLeft()) === 0) {
       return;
     }
   }
 
-  @Process({
-    concurrency: GLOBAL_DOWNLOADS_CONCURRENCY,
-  })
+  @Process({ concurrency: GLOBAL_DOWNLOADS_CONCURRENCY })
   async doDownload(job: Job) {
     return this.downloadsService.download({
       url: job.data.url,
