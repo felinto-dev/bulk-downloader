3d076f164593dfb58a4b94710bb065133ead6aef
Felinto <emersong20.email@gmail.com>
2022-02-23 02:17:17 +0000

    🔇 remove prisma queries

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,14 +1,8 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    super({
-      log: ['query'],
-    });
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
