2e93edcc3f4c9a1fe743c544cca0f712af3028ae
Felinto <emersong20.email@gmail.com>
2022-03-25 19:54:43 -0300

    💡 add some comments for improve code legibility

--- src/inputs/upsert-hoster.input.ts
+++ src/inputs/upsert-hoster.input.ts
@@ -14,22 +14,28 @@ import {
 import { HosterLimits } from '../dto/hoster-limits.dto';
 
 export class UpsertHosterInput {
+  /** The unique ID for Hoster (e.g "wp.org") */
+  @IsDefined()
   @Matches(/^([a-z0-9-_]+)$/)
   id: string;
 
+  /** Name of the hoster (e.g. "wordpress.org") */
   @IsNotEmpty()
   name: string;
 
+  /** how many concurrent downloads are allowed  */
   @Min(0)
   @Max(GLOBAL_DOWNLOADS_CONCURRENCY)
   @IsNumber()
   concurrencyConnections: number;
 
+  /** Limits for the hoster (e.g. max. downloads allowed per day) */
   @IsDefined()
   @ValidateNested()
   @Type(() => HosterLimits)
   limits: HosterLimits;
 
+  /** Authentication method for the hoster (e.g. "basic") */
   @IsEnum(HosterAuthenticationMethod)
   credentialsStrategy: HosterAuthenticationMethod;
 }
