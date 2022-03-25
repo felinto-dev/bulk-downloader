5aebfc117be3212045ba3149e0cbe738e6f16017
Felinto <emersong20.email@gmail.com>
2022-03-25 12:00:41 -0300

    ⚰️ remove dead code

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,4 @@
 import { DownloadsController } from './downloads.controller';
-import { HealthController } from './health.controller';
 import { HostersController } from './hosters.controller';
 
-export const CONTROLLERS = [
-  DownloadsController,
-  HealthController,
-  HostersController,
-];
+export const CONTROLLERS = [DownloadsController, HostersController];
