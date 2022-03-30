c6eee79c7cee31445cae289dfa3f64c3ffc129dd
Felinto <emersong20.email@gmail.com>
2022-03-30 11:14:23 -0300

    ⚰️ remove dead code

--- src/utils/objects.ts
+++ src/utils/objects.ts
@@ -1,20 +1,5 @@
-const removeNullObjectValues = (obj: object): object =>
-  Object.entries(obj || {}).reduce(
-    (a, [k, v]) => (v === null ? a : ((a[k] = v), a)),
-    {},
-  );
-
-export const subtractObjects = <K extends PropertyKey>(
-  objA: Record<K, number>,
-  objB: Record<K, number>,
-) =>
-  (Object.keys(removeNullObjectValues(objA)) as K[]).reduce((a, k) => {
-    a[k] = objA[k] - objB[k];
-    return a;
-  }, {} as { [P in K]: number });
-
 export const getMinValueFromObjectValues = (obj: object): number =>
-  Math.min(...Object.values(removeNullObjectValues(obj) || {}));
+  Math.min(...Object.values(obj || {}).filter((v) => v !== null));
 
 export const checkIfNumberExistsInObjectValues = (
   obj: object,
