ab3c4a6dbe1c280c6ab5830c40993eef7de333b0
Felinto <emersong20.email@gmail.com>
2022-04-02 03:03:20 -0300

    🩹 access repository directly on interator

--- src/iterators/pending-download.interator.ts
+++ src/iterators/pending-download.interator.ts
@@ -1,16 +1,37 @@
 import { PendingDownload } from '@/database/interfaces/pending-download';
-import { DownloadsService } from '@/services/downloads.service';
+import { PrismaService } from '@/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { DownloadStatus } from '@prisma/client';
 
 @Injectable()
 export class PendingDownloadsIterator {
-  constructor(private readonly downloadsService: DownloadsService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   async hasMore(): Promise<boolean> {
     return !!this.next();
   }
 
   async next(): Promise<PendingDownload> {
-    return this.downloadsService.findPendingDownload();
+    return this.prisma.download.findFirst({
+      orderBy: [
+        { Hoster: { maxConcurrentDownloads: 'asc' } },
+        { priority: 'desc' },
+      ],
+      where: {
+        status: DownloadStatus.PENDING,
+        // TODO: Not look for hosters that have reached their concurrency limit
+        Hoster: {
+          limits: { quotaRenewsAt: { lt: new Date() } },
+        },
+      },
+      select: {
+        url: true,
+        downloadId: true,
+        hosterId: true,
+        Hoster: {
+          select: { maxConcurrentDownloads: true },
+        },
+      },
+    });
   }
 }
