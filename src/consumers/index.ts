e317d848d574a64ed2d62b55c15a098f155d37f0
Felinto <emersong20.email@gmail.com>
2022-04-02 03:06:17 -0300

    🐛 add downloads orchestrating to consumers

--- src/consumers/index.ts
+++ src/consumers/index.ts
@@ -1,7 +1,9 @@
+import { DownloadsOrchestratingConsumer } from './downloads-orchestrating.consumer';
 import { DownloadsProcessingConsumer } from './downloads-processing.consumers';
 import { DownloadsSortingConsumer } from './downloads-sorting.consumer';
 
 export const CONSUMERS = [
   DownloadsProcessingConsumer,
   DownloadsSortingConsumer,
+  DownloadsOrchestratingConsumer,
 ];
