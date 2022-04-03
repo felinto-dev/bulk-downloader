1ad66cb65a49af099dcd03a055b3d5a7aba4fe45
Felinto <emersong20.email@gmail.com>
2022-04-03 03:50:04 -0300

    ✅ remove dead code

--- src/orchestrators/__tests__/downloads-enqueue.orchestrator.spec.ts
+++ src/orchestrators/__tests__/downloads-enqueue.orchestrator.spec.ts
@@ -1,13 +1,10 @@
 import { DOWNLOADS_PROCESSING_QUEUE } from '@/consts/queues';
-import { PendingDownload } from '@/database/interfaces/pending-download';
 import { DownloadJobDto } from '@/dto/download.job.dto';
 import { PendingDownloadsIterator } from '@/iterators/pending-download.interator';
-import { HosterQuotasService } from '@/services/hoster-quotas.service';
 import { createMock } from '@golevelup/ts-jest';
 import { getQueueToken } from '@nestjs/bull';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Queue } from 'bull';
-import { HosterDownloadsConcurrencyValidator } from '../../validators/concurrent-hoster-downloads.validator';
 import { DownloadsEnqueueOrchestrator } from '../downloads-enqueue.orchestrator';
 
 describe(DownloadsEnqueueOrchestrator.name, () => {
@@ -15,9 +12,6 @@ describe(DownloadsEnqueueOrchestrator.name, () => {
   let queue: Queue<DownloadJobDto>;
 
   const mockedQueue = createMock<Queue<DownloadJobDto>>();
-  const mockedHosterQuotasService = createMock<HosterQuotasService>();
-  const mockedConcurrentHosterDownloadsOrchestrator =
-    createMock<HosterDownloadsConcurrencyValidator>();
   const mockedPendingDownloadsIterator = createMock<PendingDownloadsIterator>();
 
   beforeEach(async () => {
@@ -27,14 +21,6 @@ describe(DownloadsEnqueueOrchestrator.name, () => {
         {
           provide: getQueueToken(DOWNLOADS_PROCESSING_QUEUE),
           useValue: mockedQueue,
-        },
-        {
-          provide: HosterQuotasService,
-          useValue: mockedHosterQuotasService,
-        },
-        {
-          provide: HosterDownloadsConcurrencyValidator,
-          useValue: mockedConcurrentHosterDownloadsOrchestrator,
         },
         {
           provide: PendingDownloadsIterator,
@@ -57,39 +43,6 @@ describe(DownloadsEnqueueOrchestrator.name, () => {
     expect(service).toBeDefined();
   });
 
-  describe(DownloadsEnqueueOrchestrator.prototype.canDownloadNow.name, () => {
-    const mockedPendingDownload: PendingDownload = {
-      downloadId: 'downloadId',
-      hosterId: 'hosterId',
-      url: 'url',
-    };
-
-    it('should return false if the hoster quota is reached', async () => {
-      mockedHosterQuotasService.hasReachedQuota.mockResolvedValue(true);
-      mockedConcurrentHosterDownloadsOrchestrator.hasReachedConcurrentDownloadsLimit.mockResolvedValue(
-        false,
-      );
-      const result = await service.canDownloadNow(mockedPendingDownload);
-      expect(result).toBe(false);
-    });
-    it('should return false if the hoster can not do +1 concurrent download', async () => {
-      mockedHosterQuotasService.hasReachedQuota.mockResolvedValue(false);
-      mockedConcurrentHosterDownloadsOrchestrator.hasReachedConcurrentDownloadsLimit.mockResolvedValue(
-        true,
-      );
-      const result = await service.canDownloadNow(mockedPendingDownload);
-      expect(result).toBe(false);
-    });
-    it('should return true when the hoster has not reached quota and neither concurrent downloads limit', async () => {
-      mockedHosterQuotasService.hasReachedQuota.mockResolvedValue(false);
-      mockedConcurrentHosterDownloadsOrchestrator.hasReachedConcurrentDownloadsLimit.mockResolvedValue(
-        false,
-      );
-      const result = await service.canDownloadNow(mockedPendingDownload);
-      expect(result).toBe(true);
-    });
-  });
-
   describe(DownloadsEnqueueOrchestrator.prototype.run.name, () => {
     it('should abort where there are no pending downloads in database', async () => {
       mockedPendingDownloadsIterator.hasNext.mockResolvedValueOnce(false);
@@ -107,7 +60,6 @@ describe(DownloadsEnqueueOrchestrator.name, () => {
           url: 'url',
         })
         .mockReturnValueOnce(null);
-      service.canDownloadNow = jest.fn().mockResolvedValueOnce(true);
       await service.run();
       expect(queue.add).toHaveBeenCalled();
     });
